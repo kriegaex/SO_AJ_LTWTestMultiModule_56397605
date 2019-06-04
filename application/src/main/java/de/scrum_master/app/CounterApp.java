249f7a07687d455f44a83f40fcc45b1389de882d
Alexander Kriegisch <Alexander@Kriegisch.name>
2019-06-04 08:06:18 +0700

    Add Maven Exec plugin in order to show how to start application + LTW

---
+++ application/src/main/java/de/scrum_master/app/CounterApp.java
@@ -0,0 +1,15 @@
+package de.scrum_master.app;
+
+import spark.Spark;
+
+import static spark.Spark.get;
+
+public class CounterApp {
+  public static void main(String[] args) {
+    get("/counter", (req, res) -> {
+      new MyCounter().count();
+      return "I ALREADY CALLED COUNTER ASPECT ON METHOD:MyCounter().count()";
+    });
+    System.out.println("Application starting on port :" + Spark.port());
+  }
+}
